7d30517c343eafaf62a411db88f79f4c7629892a
Adam Reid <adameastcoast@gmail.com>
2017-05-28 00:48:56 -0400

    Clean up interfaces

--- src/main/java/com/training/adapter/AdvancedMediaPlayer.java
+++ src/main/java/com/training/adapter/AdvancedMediaPlayer.java
@@ -4,6 +4,6 @@ package com.training.adapter;
  * Created by adame on 5/28/2017.
  */
 public interface AdvancedMediaPlayer {
-    public void playVlc(String filename);
-    public void playMp4(String filename);
+    void playVlc(String filename);
+    void playMp4(String filename);
 }
--- src/main/java/com/training/adapter/MediaPlayer.java
+++ src/main/java/com/training/adapter/MediaPlayer.java
@@ -4,5 +4,5 @@ package com.training.adapter;
  * Created by adame on 5/28/2017.
  */
 public interface MediaPlayer {
-    public void play(String audioType, String filename);
+    void play(String audioType, String filename);
 }
