af5b1261ef4d6e1d36efc811316f1861fec5e9fd
Adam Reid <adameastcoast@gmail.com>
2017-05-28 00:22:41 -0400

    Add the advanced media player interface

---
+++ src/main/java/com/training/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,9 @@
+package com.training.adapter;
+
+/**
+ * Created by adame on 5/28/2017.
+ */
+public interface AdvancedMediaPlayer {
+    public void playVlc(String filename);
+    public void playMp4(String filename);
+}
