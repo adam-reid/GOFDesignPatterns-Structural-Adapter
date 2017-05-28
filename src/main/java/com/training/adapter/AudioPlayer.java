8f26401f292fd83ab8ad2a966015eace11c7d9dd
Adam Reid <adameastcoast@gmail.com>
2017-05-28 01:02:13 -0400

    Spice up the mp3

--- src/main/java/com/training/adapter/AudioPlayer.java
+++ src/main/java/com/training/adapter/AudioPlayer.java
@@ -8,7 +8,7 @@ public class AudioPlayer implements MediaPlayer {
     @Override
     public void play(String audioType, String filename) {
         if(audioType.equalsIgnoreCase("mp3")) {
-            System.out.println("Jamming on the mp3!");
+            System.out.println("Jamming on the mp3, " + filename + "!");
         }
         else if(audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
             new MediaAdapter().play(audioType,filename);
