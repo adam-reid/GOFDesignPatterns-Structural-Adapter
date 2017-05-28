ecdbae4d84bb9ddf40fe817297d6e001f36be31e
Adam Reid <adameastcoast@gmail.com>
2017-05-28 01:00:58 -0400

    Simplify.

--- src/main/java/com/training/adapter/AudioPlayer.java
+++ src/main/java/com/training/adapter/AudioPlayer.java
@@ -4,7 +4,6 @@ package com.training.adapter;
  * Created by adame on 5/28/2017.
  */
 public class AudioPlayer implements MediaPlayer {
-    MediaAdapter mediaAdapter;
 
     @Override
     public void play(String audioType, String filename) {
@@ -12,8 +11,7 @@ public class AudioPlayer implements MediaPlayer {
             System.out.println("Jamming on the mp3!");
         }
         else if(audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
-            mediaAdapter = new MediaAdapter(audioType);
-            mediaAdapter.play(audioType, filename);
+            new MediaAdapter().play(audioType,filename);
         }
         else {
             System.out.println("No music for you!");
--- src/main/java/com/training/adapter/MediaAdapter.java
+++ src/main/java/com/training/adapter/MediaAdapter.java
@@ -5,33 +5,15 @@ package com.training.adapter;
  */
 public class MediaAdapter implements MediaPlayer{
 
-    AdvancedMediaPlayer advancedMediaPlayer;
-
-    public MediaAdapter(String audioType) {
-        if(audioType == null) {
-            System.out.println("Missing audio type.");
-        }
-        else if(audioType.equalsIgnoreCase("vlc")) {
-            advancedMediaPlayer = new VlcPlayer();
-
-        }
-        else if(audioType.equalsIgnoreCase("mp4")) {
-            advancedMediaPlayer = new Mp4Player();
-        }
-        else {
-            System.out.println("Unsupported audio type!");
-        }
-    }
-
     @Override
     public void play(String audioType, String filename) {
         //note: i don't like this implementation.. why would you allow this? you already did it
         //in the constructor..
         if(audioType.equalsIgnoreCase("vlc")) {
-            advancedMediaPlayer.playVlc(filename);
+            new VlcPlayer().playVlc(filename);
         }
         else if (audioType.equalsIgnoreCase("mp4")) {
-            advancedMediaPlayer.playMp4(filename);
+            new Mp4Player().playMp4(filename);
         }
         else {
             System.out.println("no music for you!");
